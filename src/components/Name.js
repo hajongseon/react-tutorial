d1a7629d6368aabc5a035182f1ca7f6ad4668656
HaJongSeon <whdtjs5099@gmail.com>
2021-08-26 19:17:51 +0900

    rupy

---
+++ src/components/Name.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import '../App.css';
+
+
+function Name() {
+    return (
+        <div className="qwe">
+        <h1>wewewrr</h1>
+        </div>
+    );
+}
+
+export default Name;
